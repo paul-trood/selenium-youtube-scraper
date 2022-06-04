4228a5509a5434f41a38ab514a5ddfe676701da5
paultrood <91702175+paul-trood@users.noreply.github.com>
2022-06-04 16:20:49 +0000

    changed string formatting for csv naming

--- scraper.py
+++ scraper.py
@@ -4,16 +4,19 @@ import json
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-"""Error with extracting all the videos.
-Time command allows for the web page to completely load, and extract all elements"""
+
+"""There is an error with extracting all the videos when not given enought time for the page to completely load.
+Time module allows for the web page to completely load, and extract all elements"""
 import time
 
-import pandas as pd
+import datetime
+import pytz
+
 import smtplib
+import pandas as pd
 
 # YouTube webpage url for the top trending videos
 youtube_url = "https://www.youtube.com/feed/trending"
-
 
 # Chromium driver options
 def get_driver():
@@ -23,7 +26,6 @@ def get_driver():
     chrome_options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(options=chrome_options)
     return driver
-
 
 def get_videos(driver):
     video_div_tag = "ytd-video-renderer"
@@ -35,41 +37,33 @@ def get_videos(driver):
     videos = driver.find_elements(By.TAG_NAME, video_div_tag)
     return videos
 
-
 # Parse all the videos of top trending youtube
 def parse_video(video):
     title_tag = video.find_element(By.ID, "video-title")
     title = title_tag.text
     url = title_tag.get_attribute("href")
-
-    thumbnail_tag = video.find_element(By.TAG_NAME, "img")
-    thumbnail_url = thumbnail_tag.get_attribute("src")
-
     channel_div = video.find_element(By.CLASS_NAME, "ytd-channel-name")
     channel_name = channel_div.text
 
     description = video.find_element(By.ID, "description-text").text
-
+  
 # Iterate through videos to extract the views and the date posted
     views = video.find_element(By.XPATH,'.//*[@id="metadata-line"]/span[1]').text
     post_date = video.find_element(By.XPATH,'.//*[@id="metadata-line"]/span[2]').text
 
-    date_scraped = pd.Timestamp("today").strftime('%Y-%m-%d %H:%M:%S')
-
-    
+# Current time (local timezone) of the webscraping
+    date_scraped = datetime.datetime.now(pytz.timezone('US/Eastern')).strftime('%Y-%m-%d %H:%M:%S')
 
 # Return a dictionary of the scraped elements
     return {
         "title": title,
         "url": url,
-        "thumbnail_url": thumbnail_url,
         "channel": channel_name,
         "description": description,
         "views": views,
         "post_date": post_date,
         "date_scraped":date_scraped
     }
-
 
 def send_email(body):
   try:
@@ -116,11 +110,12 @@ if __name__ == "__main__":
 
   print(videos_data)
 
-    # Save the dictionary to a pandas dataframe
-  print("Save data to csv using pandas")
+  # Save the dictionary to a pandas dataframe
   videos_dataframe = pd.DataFrame(videos_data)
-  print(videos_dataframe)
-  videos_dataframe.to_csv("trending.csv", index=None)
+  
+  # Format the date of scraping, local time is united states eastern timezone
+  date = pd.Timestamp("today", tz ="US/Eastern").strftime('%Y-%m-%d')
+  videos_dataframe.to_csv(f"trending_{date}.csv", index=None)
 
   print("Send an email with the results")
   body = json.dumps(videos_data, indent = 2)
